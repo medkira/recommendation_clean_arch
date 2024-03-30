95198574f274e508f17ee794e784513370b4faf6
medkira <mohamed.deftex@gmail.com>
2024-03-30 18:36:30 +0100

    add: cors middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -6,13 +6,13 @@ import { setupViewEngine } from "./view-engine";
 
 export const setupApp = (): Express => {
     const app = express();
-
     // ? put anything that the express will use
-
     setupViewEngine(app);
     setupSocket(app);
     setupMiddleware(app);
     setupRoutes(app);
+
+
 
     return app;
 }
--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,10 +1,13 @@
 import { jsonparser, urlencodedparser } from "@main/middlewares/body-parser"
+import cors from "cors";
 import { Express } from "express"
 
 
 export const setupMiddleware = (app: Express): void => {
     app.use(jsonparser);
     app.use(urlencodedparser);
+    app.use(cors())
+
     // app.use(session({
     //     secret: 'testttt', // Replace with a secret key for session encryption
     //     resave: false,
